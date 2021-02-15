17e909af9c7170779f3fcb9c4f48e7c2077f5677
Hrolfr <sabrina.sales163@gmail.com>
2021-02-15 11:27:44 -0300

    add icons no footer

--- js/main.js
+++ js/main.js
@@ -3,3 +3,8 @@ function evalSlider()
     var sliderValue = document.getElementById('rating').value;
     document.getElementById('sliderVal').innerHTML = sliderValue;
 }
+
+function clicou(){
+    document.getElementById("clicou()").innerHTML = "pause";
+    console.log(document.getElementById("pause"));
+}
