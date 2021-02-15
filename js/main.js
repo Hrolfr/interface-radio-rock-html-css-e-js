1961e679981e5968a98168d2197b1958de02aa82
Hrolfr <sabrina.sales163@gmail.com>
2021-02-15 11:28:29 -0300

    apaguei a funcao clicou

--- js/main.js
+++ js/main.js
@@ -3,8 +3,3 @@ function evalSlider()
     var sliderValue = document.getElementById('rating').value;
     document.getElementById('sliderVal').innerHTML = sliderValue;
 }
-
-function clicou(){
-    document.getElementById("clicou()").innerHTML = "pause";
-    console.log(document.getElementById("pause"));
-}
